3c0fe2c94b79efd75b66f59b60e80bef36b8d923
Suvam Debnath <debnathsuvam322@gmail.com>
2025-10-08 08:21:34 +0530

    setting up two consumer with same topic and same group

--- consumer/src/main/java/com/suvam/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/suvam/consumer/KafkaConsumer.java
@@ -5,8 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
+
     @KafkaListener(topics = "my-topic" , groupId = "my-new-group")
-    public void listen(String message){
-        System.out.println("Received Message: "+ message);
+    public void listen1(String message){
+        System.out.println("Received Message in 1st consumer: "+ message);
+    }
+
+    @KafkaListener(topics = "my-topic" , groupId = "my-new-group")
+    public void listen2(String message){
+        System.out.println("Received Message in 2nd consumer: "+ message);
     }
 }
